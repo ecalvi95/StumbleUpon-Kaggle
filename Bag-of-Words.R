a25d31c8d3a3f0f6f5151014edfd276f885e69be
Erica Calvi <96635156+ecalvi95@users.noreply.github.com>
2022-02-08 07:22:12 -0500

    Update Bag-of-Words.R

--- Bag-of-Words.R
+++ Bag-of-Words.R
@@ -58,10 +58,6 @@ word_list = df %>% unnest_tokens(word, 'boilerplate') %>%
   filter(n >= 500) %>% #remove less than 500
   pull(word)
 
-#I think bag_of_words is probably your best bet for association rules
-#it's just really big and hard to navigate to check how it works
-#Lemme know what you think! 
-
 bag_of_words = df %>% unnest_tokens(word, 'boilerplate') %>%
   filter(word %in% word_list) %>%
   count(ID, word) %>%
@@ -95,11 +91,6 @@ ratio = df %>%
   select(word, ratio)
 
 View(ratio)
-#top words -> sinus, springform, gnocchi, dumplings
-#if you search them in the words_plus_labels table - you can see that
-#they basically don't have any non evergreen labels - always considered relevent when used
-
-#finding the most informative features
 
 
 #identifies the top 10 words in ratio and bottom 10 words
@@ -112,14 +103,8 @@ clean_counts <-counts %>% # takes the count table and removes filler words
 View(clean_counts)
 
 ggplot(clean_counts, aes(x= rank, y = n)) + geom_point() + scale_x_log10() + scale_y_log10()
-#the first dot is "1", first dot is "2", third dot "body"
-#of course we don't consider these words to be relevant, if we felt the chart would be useful for something
-#we could try to remove those first words before "recipe"
 
 #bunch of food words
-# I think "cup" goes with "world" a little bit lower --> World Cup -> there was one in 2008
-# not sure if we want to explore other options outside food
-#perhaps we can find a few other topics that people found interesting
 
 library(data.table)
 fwrite(bag_of_words,"~/Babson/Spring/MSB6300/Intro Project/bag_of_words_500.csv" )
